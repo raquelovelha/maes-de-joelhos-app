5f12ed00fef9523eb8591ad5bc81a0b6117edb55
raquelovelha <raquel.guerreiro@mpc.org.br>
2026-03-18 19:02:04 -0300

    Fix: corrigir import do firebase em Register.tsx

--- src/paginas/Register.tsx
+++ src/paginas/Register.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { auth, db } from '../firebase'; 
+import { auth, db } from '../services/firebase';
 import { 
   createUserWithEmailAndPassword, 
   signInWithEmailAndPassword, 
