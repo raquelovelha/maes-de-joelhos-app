f0d487e0bea7d9239269adcf900117ff1af5dc3e
raquelovelha <raquel.guerreiro@mpc.org.br>
2026-03-18 20:43:27 -0300

    fix: corrigindo caminho do firebase no Timer

--- src/paginas/Timer.tsx
+++ src/paginas/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from '../firebase';
+import { db } from '../services/firebase';
 import { collection, addDoc, serverTimestamp, doc, updateDoc } from 'firebase/firestore';
 import { Prayer } from '../types';
 
